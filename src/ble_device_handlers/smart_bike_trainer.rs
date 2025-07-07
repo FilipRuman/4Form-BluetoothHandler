e7c78036ce2f1655ea8bde6600be2925966c6e2a
FR <137614590+FilipRuman@users.noreply.github.com>
2025-07-07 16:03:41 +0200

    made slope control work + some small improvements

--- src/ble_device_handlers/smart_bike_trainer.rs
+++ src/ble_device_handlers/smart_bike_trainer.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use btleplug::api::{Characteristic, Peripheral, WriteType, bleuuid::uuid_from_u16};
 use futures::StreamExt;
 use tokio::net::TcpStream;
@@ -5,10 +6,7 @@ use uuid::Uuid;
 
 use crate::{ble_device_handlers::*, tcp_parser};
 
-pub(crate) fn parse_indoor_bike_data(data: &[u8]) -> (u16, u16, u16) {
-
-    // let flags = u16::from_le_bytes([data[0], data[1]]);
-
+pub(crate) fn parse_data(data: &[u8]) -> (u16, u16, u16) {
     // Seems right but not accurate to ftms spec  XD
     let wheel_rotation = u16::from_le_bytes([data[2], data[3]]);
     let cadence = u16::from_le_bytes([data[4], data[5]]);
@@ -18,30 +16,25 @@ pub(crate) fn parse_indoor_bike_data(data: &[u8]) -> (u16, u16, u16) {
     (power, cadence, wheel_rotation)
 }
 
-pub async fn handle_smart_trainer_peripheral(
-    control_char: &Characteristic,
-    data_char: &Characteristic,
+pub async fn handle_peripheral(
     peripheral: &btleplug::platform::Peripheral,
     stream: &mut TcpStream,
+    control_char: &Characteristic,
 ) -> Result<()> {
     let mut notifications = peripheral.notifications().await?;
     while let Some(notification) = notifications.next().await {
-        let output_data = parse_indoor_bike_data(&notification.value);
+        let output_data = parse_data(&notification.value);
         let current_power = output_data.0;
         let cadence = output_data.1;
         let wheel_rotation = output_data.2;
 
         tcp_parser::send_bike_trainer_data(stream, current_power, cadence, wheel_rotation).await;
-
     }
     Ok(())
 }
 const FTMS_CONTROL_POINT: Uuid = uuid_from_u16(0x2AD9);
 const FTMS_DATA_READ_POINT: Uuid = uuid_from_u16(0x2AD2);
-pub async fn get_smart_trainer_device(
-    peripheral: btleplug::platform::Peripheral,
-    peripheral_index: usize,
-) -> Result<BleDevice> {
+pub async fn get_device(peripheral: btleplug::platform::Peripheral) -> Result<BleDevice> {
     connect_to_peripheral(&peripheral)
         .await
         .context("connecting to smart trainer")?;
@@ -53,40 +46,45 @@ pub async fn get_smart_trainer_device(
     let data_char = get_characteristic_with_uuid(FTMS_DATA_READ_POINT, &peripheral)
         .context("Data read characteristic not found")?;
 
-    let start_cmd = vec![0x07]; // 0x07 = Start or Resume Training
-    peripheral
-        .write(&control_char, &start_cmd, WriteType::WithResponse)
-        .await
-        .context("starting training command:")?;
-
     peripheral
         .subscribe(&data_char)
         .await
         .context("subscribing to trainer data notifications")?;
+
+    peripheral
+        .write(&control_char, &[0x00], WriteType::WithResponse)
+        .await
+        .context("request control command:")?;
+    peripheral
+        .write(&control_char, &[0x07], WriteType::WithResponse)
+        .await
+        .context("starting training command:")?;
+    set_target_slope(20, &peripheral, &control_char)
+        .await
+        .context("setting target slope")?;
     Ok(BleDevice::SmartTrainer {
         control_char,
         data_char,
         peripheral,
     })
 }
-
-async fn set_target_power(
-    target_power: u16,
-
+pub async fn set_target_slope(
+    slope_percent: i16,
     peripheral: &btleplug::platform::Peripheral,
     control_char: &Characteristic,
-) {
-    let cmd = vec![
-        0x04, // Opcode: Set Target Power
-        (target_power & 0xFF) as u8,
-        (target_power >> 8) as u8,
+) -> Result<()> {
+    let target = slope_percent;
+    let payload = vec![
+        0x05,                         // Set Target Inclination opcode
+        (target & 0xFF) as u8,        // LSB
+        ((target >> 8) & 0xFF) as u8, // MSB
     ];
-    println!("Sending resistance command");
-    let res = peripheral
-        .write(&control_char, &cmd, WriteType::WithResponse)
-        .await;
-    match res {
-        Ok(_) => println!("Write successful"),
-        Err(e) => println!("Write failed: {:?}", e),
-    }
+    let response = peripheral
+        .write(control_char, &payload, WriteType::WithResponse)
+        .await?;
+    info!(
+        "successfully set target slope: {}% response: {:?}",
+        slope_percent, response
+    );
+    Ok(())
 }
--- src/tcp/tcp_parser.rs
+++ src/tcp/tcp_parser.rs
@@ -6,6 +6,10 @@ use crate::{
 use spdlog::prelude::*;
 use tokio::net::TcpStream;
 
+pub async fn send_smart_watch_data(stream: &mut TcpStream, hr: u8) {
+    tcp::send_tcp_data(stream, format!("h hr:{};", hr)).await;
+}
+
 pub async fn send_bike_trainer_data(
     stream: &mut TcpStream,
     power: u16,
@@ -25,6 +29,7 @@ pub async fn send_bike_trainer_data(
 pub async fn handle_data_input_from_tcp(
     data: &str,
     valid_peripherals: &[btleplug::platform::Peripheral],
+    devices: &Vec<BleDevice>,
 
     stream: &mut TcpStream,
 ) -> Option<BleDevice> {
@@ -39,16 +44,25 @@ pub async fn handle_data_input_from_tcp(
     // this would only fail if len == 0 but it is checked
 
     match chars.next().unwrap() {
-        'i' => match peripherals_tcp_parser::handle_parsing_peripheral_connection(
-            data,
-            valid_peripherals,
-            stream,
-        )
-        .await
-        {
-            Ok(option_value) => option_value,
+        'i' => {
+            match peripherals_tcp_parser::handle_parsing_peripheral_connection(
+                data,
+                valid_peripherals,
+                stream,
+            )
+            .await
+            {
+                Ok(option_value) => option_value,
+                Err(error) => {
+                    error!("handling parsing peripheral: {error}");
+                    None
+                }
+            }
+        }
+        's' => match peripherals_tcp_parser::handle_slope_control(devices, data).await {
+            Ok(_) => None,
             Err(error) => {
-                error!("handling parsing peripheral: {error}");
+                error!("error while setting slope {error}");
                 None
             }
         },
